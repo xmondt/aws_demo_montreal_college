739f36d364747d259c354f1d237edce11bf6aad6
Sami Islam <islam.samipkr@gmail.com>
2024-01-22 17:20:36 -0500

    Update ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance""myec2m"{
     ami=data.aws_ami.amz_linux2.id
     instance_type = var.instance_type
-    vpc_security_group_ids = [aws_security_group.vpc-ssh,aws_security_group.vpc-web]
+    vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
     count = 2
     tags = {
         name="count-demo-${count.index}"
