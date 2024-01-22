5310017f965253321198869570b3e9c3e2372dc0
Sami Islam <islam.samipkr@gmail.com>
2024-01-22 16:17:27 -0500

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -4,3 +4,14 @@ variable "AWS_ACCESS_KEY"{
 variable "AWS_SECRET_ACCESS_KEY"{
   
 }
+variable "aws_region" {
+    description = "Region in which AWS Resources to be created"
+    type = string
+    default ="us-east-1"
+  
+}
+variable "instance_type"{
+    description="EC2 Instance type"
+    type =string
+    default="t3.micro"
+}
