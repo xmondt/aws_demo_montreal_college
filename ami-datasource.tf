ccf83a385fbb9775cda3ec1b3b99c83d28d54739
Sami Islam <islam.samipkr@gmail.com>
2024-01-22 16:51:53 -0500

    Update ami-datasource.tf

--- ami-datasource.tf
+++ ami-datasource.tf
@@ -1,21 +1,24 @@
-data "aws_ami""amz_linux2"{
-    most_recent = true
-    owners = ["amazon"]
-    filter{
-        name="name"
-        values=["ami-0e9107ed11be76fde"]
-    }
-    filter{
-        name="root-device-type"
-        values=["ebs"]
-    }
-    filter{
-        name="virtualization-type"
-        values=["hvm"]
-    }
-    filter{
-        name="architecture"
-        values=["x86_64"]
-    }
-}
+data "aws_ami" "amz_linux2" {
+  most_recent = true
+  owners      = ["amazon"]
 
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
