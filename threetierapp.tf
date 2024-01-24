2498a0bb171d5018b5e2dfcc9613f5a7e3ef5259
Sami Islam <islam.samipkr@gmail.com>
2024-01-24 18:27:47 -0500

    Create threetierapp.tf

---
+++ threetierapp.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"  # Set your desired AWS region
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  # Include any necessary variables for the VPC module
+}
+
+module "rds" {
+  source = "terraform-aws-modules/rds/aws"
+  # Include any necessary variables for the RDS module
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your desired AMI
+  instance_type = "t2.micro"  # Replace with your desired instance type
+
+  subnet_id     = module.vpc.public_subnets[0]  # Use one of the public subnets from the VPC module
+  key_name      = "your-key-pair"  # Replace with your key pair name
+
+  # Add any other required configurations for your EC2 instance
+}
