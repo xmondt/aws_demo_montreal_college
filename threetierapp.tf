f8ed57be74dfd82d033c408c9892bda94cc0dc67
Sami Islam <islam.samipkr@gmail.com>
2024-01-24 18:42:41 -0500

    Update threetierapp.tf

--- threetierapp.tf
+++ threetierapp.tf
@@ -1,22 +1,49 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-  # Include any necessary variables for the VPC module
+module "autoscaling" {
+  source      = "./modules/autoscaling" #A
+  namespace   = var.namespace           #B
+  ssh_keypair = var.ssh_keypair         #A
+
+  vpc       = module.networking.vpc     #A
+  sg        = module.networking.sg      #A
+  db_config = module.database.db_config #A
+
 }
 
-module "rds" {
-  source     = "terraform-aws-modules/rds/aws"
-  identifier = "my-rds-instance"  # Replace with your unique identifier
+module "database" {
+  source    = "./modules/database" #A
+  namespace = var.namespace        #B
 
-  db_family = "mysql5.7"  # Example value, replace with the appropriate RDS family
-  # Include any other necessary variables for the RDS module
+  vpc = module.networking.vpc #A
+  sg  = module.networking.sg  #A
+
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your desired AMI
-  instance_type = "t2.micro"  # Replace with your desired instance type
+module "networking" {
+  source    = "./modules/networking" #A
+  namespace = var.namespace          #B
+}
+output "db_password" {
+  value = module.database.db_config.password
+  sensitive = true
+}
 
-  subnet_id     = module.vpc.public_subnets[0]  # Use one of the public subnets from the VPC module
-  key_name      = "your-key-pair"  # Replace with your key pair name
+output "lb_dns_name" {
+  value = module.autoscaling.lb_dns_name
+}
+variable "namespace" {
+  description = "The project namespace to use for unique resource naming"
+  type        = string
+  default=""mythreetierapplication"
+}
 
-  # Add any other required configurations for your EC2 instance
+variable "ssh_keypair" {
+  description = "SSH keypair to use for EC2 instance"
+  default     = null #A
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+  type        = string
 }
