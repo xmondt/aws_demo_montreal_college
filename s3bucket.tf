e5721ee20af5b6ed582060d4acf10c07e6a138f3
xmondt <168039714+xmondt@users.noreply.github.com>
2024-06-05 18:43:25 -0400

    Update s3bucket.tf

--- s3bucket.tf
+++ s3bucket.tf
@@ -4,3 +4,23 @@ resource "aws_s3_bucket" "bucket1" {
         name="test-bucket-${count.index}"
         }
 }
+
+#Create 5 buckets and disable versioning for all
+variable "bucket_count"{
+    type=number
+    default=5
+}
+resource "aws_s3_bucket" "bucket_test" {
+    count=var.bucket_count
+    tags = {
+        name="tst-bucket-${count.index}"
+        }
+}
+ 
+resource "aws_s3_bucket_versioning" "versioning_example_test" {
+  count = var.bucket_count
+  bucket = aws_s3_bucket.bucket_test[count.index].id
+  versioning_configuration {
+    status = "Disabled"
+  }
+}
